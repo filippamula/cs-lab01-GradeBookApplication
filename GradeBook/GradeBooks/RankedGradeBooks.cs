4b01200989672164b5ae175d7b42e97a24a0b135
Filip.Pamula <Filip.Pamula@microsoft.wsei.edu.pl>
2022-03-06 10:55:15 +0100

    created ranked grade book class

---
+++ GradeBook/GradeBooks/RankedGradeBooks.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GradeBook.GradeBooks
+{
+    class RankedGradeBooks : BaseGradeBook
+    {
+        public RankedGradeBooks(string name) : base(name)
+        {
+            Type = Enums.GradeBookType.Ranked;
+        }
+    }
+}
